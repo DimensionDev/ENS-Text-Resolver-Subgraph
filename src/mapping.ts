254fc5dc0220958807e8e679fa48a3514e1e70e7
guanbinrui <guanbinrui@dimension.im>
2021-08-31 14:23:45 +0800

    :construction: wip

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,12 +1,10 @@
-import { Domain, TwitterHandle, Node } from "../generated/schema";
-import {
-  SetTextCall,
-  AddressChanged,
-} from "../generated/PublicResolveV1/Resolver";
-import { NewOwner, Transfer } from "../generated/Registrar/Registrar";
+import { Address, ByteArray } from '@graphprotocol/graph-ts';
+import { Domain, TwitterHandle } from '../generated/schema';
+import { SetTextCall, AddressChanged } from '../generated/PublicResolveV1/Resolver'
+import { NewOwner, Transfer } from '../generated/Registrar/Registrar'
 
 function toLowerCase(str: string): string {
-  let chars = str.split("");
+  let chars = str.split('');
   for (let i = 0; i < chars.length; i += 1) {
     let char = chars[i];
     let charCode = char.charCodeAt(0);
@@ -16,129 +14,103 @@ function toLowerCase(str: string): string {
       chars[i] = char;
     }
   }
-  return chars.join("");
+  return chars.join('');
 }
 
 function resolveTwitterHandle(raw: string): string {
   let raw_ = raw.trim();
-  if (raw_ == "") return "";
-  let chunks = raw_.split("/");
+  if (raw_ == '') return '';
+  let chunks = raw_.split('/');
   let handle = chunks[chunks.length - 1];
-  return toLowerCase(handle[0] == "@" ? handle.substr(1) : handle);
+  return toLowerCase(handle[0] == '@' ? handle.substr(1) : handle);
+}
+
+function createDomain(id: string): Domain {
+  let domain = Domain.load(id);
+  if (domain == null) {
+    domain = new Domain(id);
+  }
+  return domain as Domain;
+}
+
+function createTwitterHandle(raw: string): TwitterHandle {
+  let handle = resolveTwitterHandle(raw);
+  let twitterHandle = TwitterHandle.load(handle);
+  if (!twitterHandle) {
+    twitterHandle = new TwitterHandle(handle);
+    twitterHandle.raw = raw;
+    twitterHandle.domains = [];
+  }
+  return twitterHandle as TwitterHandle;
 }
 
 /**
  * Update text record
- * @param call
- * @returns
+ * @param call 
+ * @returns 
  */
 export function handleSetText(call: SetTextCall): void {
   let key = call.inputs.key;
-  if (
-    !key.includes("twitter") &&
-    !key.includes("Twitter") &&
-    !key.includes("TWITTER")
-  )
-    return;
+  if (!key.includes('twitter') && !key.includes('Twitter') && !key.includes('TWITTER')) return;
 
-  // retrieve node
+  // retrieve entities
   let id = call.inputs.node.toHexString();
-  let node = Node.load(id);
-  if (node == null) {
-    node = new Node(id);
-  }
-
-  // retrieve handle
   let raw = call.inputs.value;
-  let handle = resolveTwitterHandle(raw);
-  let twitterHandle = TwitterHandle.load(handle);
-  if (twitterHandle == null) {
-    twitterHandle = new TwitterHandle(handle);
-    twitterHandle.raw = raw;
-    twitterHandle.nodes = [];
-  }
+  let domain = createDomain(id);
+  let twitterHandle = createTwitterHandle(raw);
 
-  // update now
-  node.save();
+  // update domain
+  domain.save();
 
   // update twitter handle
-  let nodes = twitterHandle.nodes;
-  if (!nodes.includes(node.id)) nodes.push(node.id);
-  twitterHandle.nodes = nodes;
+  let domains = twitterHandle.domains;
+  if (!domains.includes(domain.id)) domains.push(domain.id);
+  twitterHandle.domains = domains;
   twitterHandle.save();
 }
 
 /**
  * New subnode domain found
- * @param event
+ * @param event 
  */
 export function handleNewOwner(event: NewOwner): void {
-  // retrieve node
+  // retrieve entities
   let id = event.params.node.toHexString();
-  let node = Node.load(id);
-  if (node == null) {
-    node = new Node(id);
-  }
-
-  // retrieve domain
-  let domain = Domain.load(id);
-  if (domain == null) {
-    domain = new Domain(id);
-  }
+  let domain = createDomain(id);
 
   // update domain
   domain.owner = event.params.owner;
   domain.save();
-
-  // update node
-  node.domain = id;
-  node.save();
 }
 
 /**
  * Transfer domain owner
- * @param event
- * @returns
+ * @param event 
+ * @returns 
  */
 export function handleTransfer(event: Transfer): void {
-  // retrieve node
+  // retrieve entities
   let id = event.params.node.toHexString();
-  let node = Node.load(id);
-  if (node == null) {
-    node = new Node(id);
-  }
-
-  // retrieve domain
-  let domain = Domain.load(id);
-  if (domain == null) {
-    domain = new Domain(id);
-  }
+  let domain = createDomain(id);
 
   // update domain
   domain.owner = event.params.owner;
   domain.save();
-
-  // update node
-  node.domain = id;
-  node.save();
 }
 
 /**
  * Set ETH address
- * @param event
- * @returns
+ * @param event 
+ * @returns 
  */
 export function handleAddressChanged(event: AddressChanged): void {
-  // retrieve domain
+  // retrieve entities
   let id = event.params.node.toHexString();
-  let domain = Domain.load(id);
-  if (domain == null) {
-    domain = new Domain(id);
-  }
+  let domain = createDomain(id);
 
   // update domain
-  let coinType = event.params.coinType.toI32();
-  if (coinType != 60) return;
+  if (!domain) return;
+  if (event.params.coinType.toI32() != 60) return;
   domain.eth_address = event.params.newAddress;
   domain.save();
 }
